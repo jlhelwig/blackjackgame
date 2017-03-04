8c036a79714056d2bd189bb59ecb2b757e6f4886
jlhelwig <jeff.helwig@gmail.com>
2017-03-04 12:41:37 -0600

    json and card returned, need to fix asynchronous problems

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+var theDeck
+var cardCalled
+var cardImage
+//This gets a deck I.D. we will use the same deck I.D. until the deck is down to 8 cards or so...
+$.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').done(function(deck){
+  var theDeck = deck.deck_id
+  console.log(theDeck)
+  cardDraw(theDeck)
+});
+function cardDraw(theDeck){  $.get('https://deckofcardsapi.com/api/deck/' + theDeck + '/draw/?count=1').done(function(card) {
+  // console.log(card)
+  var didItWork = card.success
+  console.log(didItWork)
+  var cardCalled = card.cards[0].value
+  var cardImage = card.cards[0].image
+  // console.log(cardImage)
+  // $(".card").(`<img =${cardImage}>`)
+  // $(".pic").append("src=" +cardImage);
+  // console.log(cardCalled)
+  console.log(cardCalled)
+  console.log(cardImage)
 
-console.log("Here too")
+});
+}
 
-
+// console.log(cardImage)
+//This will draw a card and show it (should be a reusable function for every time a card is drawn)
 
 
 
