761987f56b92e88a7aa985115d5c27d0aca428f5
jlhelwig <jeff.helwig@gmail.com>
2017-03-06 13:31:59 -0600

    refactor deck

--- script.js
+++ script.js
@@ -12,49 +12,36 @@ $(document).ready(function() {
         // console.log(theDeck)
         cardDraw(theDeck)
     });
-
+//draws 52 Random cards from above deck
     function cardDraw(theDeck) {
         $.get('https://deckofcardsapi.com/api/deck/' + theDeck + '/draw/?count=52').done(function(card) {
-          // console.log('asdfa', card);
-            // console.log(card)
-            var didItWork = card.success
-            // console.log(didItWork)
-            // for each(theDeck){
-            //     for each (var item in obj) {
-            //   sum += item;
-            // }
+
 
             var deck = []
             for (let i = 0; i < 52; i++) {
+//gives value to aces
                 if (card.cards[i].value === "ACE") {
                     card.cards[i].value = '11'
-                    cardCalled = card.cards[i].value
                 }
+//gives value to kings, queens and jacks
                 if (card.cards[i].value === "KING" || card.cards[i].value === "QUEEN" || card.cards[i].value === "JACK") {
                     card.cards[i].value = '10';
-                    cardCalled = card.cards[i].value
-                } else {
-                    cardCalled = card.cards[i].value
                 }
-                var cardImage = card.cards[i].image
-                // console.log(cardCalled)
-                // console.log(cardImage)
-                // console.log(usedCard)
-                cardObj['cardCalled'] = cardCalled;
-                cardObj['cardImage'] = cardImage;
+//adds a usedCard class set to false
                 card.cards[i].usedCard = usedCard;
-                // console.log(cardObj)
+//pushes each card into the 'deck'
                 deck.push(card.cards[i])
-                // console.log(deck)
             }
-                console.log(deck)
+//shows deck array of card objects
+            console.log(deck)
+//shows deck at i
+console.log(deck[0])
+//gives site of image
+console.log(deck[0].image)
 
-console.log(deck[0].value)
         });
-    }
 
-    // console.log(cardImage)
-    //This will draw a card and show it (should be a reusable function for every time a card is drawn)
+      }
 
 
 
