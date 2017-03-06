e7539bf04124a020732ccdd63e2a0fc7848b796e
jlhelwig <jeff.helwig@gmail.com>
2017-03-06 12:44:49 -0600

    deck built

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+    // console.log( "ready!" );
 
-var theDeck
-var cardCalled
-var cardImage
-//This gets a deck I.D. we will use the same deck I.D. until the deck is down to 8 cards or so...
-$.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').done(function(deck){
-  var theDeck = deck.deck_id
-  console.log(theDeck)
-  cardDraw(theDeck)
-});
-function cardDraw(theDeck){  $.get('https://deckofcardsapi.com/api/deck/' + theDeck + '/draw/?count=52').done(function(card) {
-  console.log(card)
-  var didItWork = card.success
-  console.log(didItWork)
-  // for each(theDeck){
-//     for each (var item in obj) {
-//   sum += item;
-// }
+    var cardObj = {}
+    var theDeck
+    var cardCalled
+    var cardImage
+    var usedCard = false
+    //This gets a deck I.D. we will use the same deck I.D. until the deck is down to 8 cards or so...
+    $.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').done(function(deck) {
+        var theDeck = deck.deck_id
+        // console.log(theDeck)
+        cardDraw(theDeck)
+    });
 
-  var cardCalled = card.cards[0].value
-  var cardImage = card.cards[0].image
-  // console.log(cardImage)
-  // $(".card").(`<img =${cardImage}>`)
-  // $(".pic").append("src=" +cardImage);
-  // console.log(cardCalled)
-  console.log(cardCalled)
-  console.log(cardImage)
+    function cardDraw(theDeck) {
+        $.get('https://deckofcardsapi.com/api/deck/' + theDeck + '/draw/?count=52').done(function(card) {
+          // console.log('asdfa', card);
+            // console.log(card)
+            var didItWork = card.success
+            // console.log(didItWork)
+            // for each(theDeck){
+            //     for each (var item in obj) {
+            //   sum += item;
+            // }
 
-});
-}
+            var deck = []
+            for (let i = 0; i < 52; i++) {
+                if (card.cards[i].value === "ACE") {
+                    card.cards[i].value = '11'
+                    cardCalled = card.cards[i].value
+                }
+                if (card.cards[i].value === "KING" || card.cards[i].value === "QUEEN" || card.cards[i].value === "JACK") {
+                    card.cards[i].value = '10';
+                    cardCalled = card.cards[i].value
+                } else {
+                    cardCalled = card.cards[i].value
+                }
+                var cardImage = card.cards[i].image
+                // console.log(cardCalled)
+                // console.log(cardImage)
+                // console.log(usedCard)
+                cardObj['cardCalled'] = cardCalled;
+                cardObj['cardImage'] = cardImage;
+                card.cards[i].usedCard = usedCard;
+                // console.log(cardObj)
+                deck.push(card.cards[i])
+                // console.log(deck)
+            }
+                console.log(deck)
 
-// console.log(cardImage)
-//This will draw a card and show it (should be a reusable function for every time a card is drawn)
+console.log(deck[0].value)
+        });
+    }
+
+    // console.log(cardImage)
+    //This will draw a card and show it (should be a reusable function for every time a card is drawn)
 
 
 
